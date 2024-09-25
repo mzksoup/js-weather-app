a397b6d4e8c33d4ac084cf21b05a2a93f96313ab
mzksoup <sei_r+git@addor.jp>
2024-09-25 15:29:27 +0900

    1st commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const weatherForm = document.getElementById("weather-form");
+const cityInput = document.getElementById("city-input");
+const weatherResults = document.getElementById("weather-results");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeather(cityInput.value);
+});
+
+function getWeather(city) {
+  weatherResults.innerHTML = `<div class="loading"></div>`;
+  fetch(`https://proxy-server-book.vercel.app/weather-data?${city}`)
+    .then((response) => response.json())
+    .then((jsonData) =>
+      (weatherResults.innerHTML = `
+          <div class="results-country">${jsonData.location.country}</div>
+          <div class="results-city">${jsonData.location.name}</div>
+          <div class="results-temp">${
+            jsonData.current.temp_c
+          }<span>℃</span></div>
+          <div class="results-condition">
+            <img src=${"https:" + jsonData.current.condition.icon} alt="icon" />
+            <span>${jsonData.current.condition.text}</span>
+          </div>`).catch((err) =>
+        alert(
+          "エラーが発生しました。ページをリロードして、もう一度トライしてください。"
+        )
+      )
+    );
+}
